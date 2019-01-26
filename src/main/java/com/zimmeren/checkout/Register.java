94ee59ce2750c96e99115b36cbc49a8f38b0b21e
unknown <zimmeren@umich.edu>
2019-01-26 18:40:12 -0500

    Create Register to track checkout total and test for no items

---
+++ src/main/java/com/zimmeren/checkout/Register.java
@@ -0,0 +1,14 @@
+package com.zimmeren.checkout;
+
+public class Register {
+
+    private float total;
+
+    public Register() {
+        total = 0.0f;
+    }
+
+    public float getTotal() {
+        return total;
+    }
+}
---
+++ src/test/java/com/zimmeren/checkout/RegisterTest.java
@@ -0,0 +1,14 @@
+package com.zimmeren.checkout;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RegisterTest {
+
+    @Test
+    public void whenRegisterPurchasesAreEmptyThenTotalIsZero() {
+        Register register = new Register();
+        assertEquals(0.0f, register.getTotal(), 0);
+    }
+}
