7af81b639cb74dd882432b4c3a5bb8b69ef70adf
unknown <zimmeren@umich.edu>
2019-01-26 22:39:31 -0500

    add markdown property and price calculation to item. test that calculate price factors in markdown

--- src/main/java/com/zimmeren/checkout/Item.java
+++ src/main/java/com/zimmeren/checkout/Item.java
@@ -4,13 +4,19 @@ public class Item {
 
     public String name;
     public float price;
+    public float markdown;
 
     private Item(String name, float price) {
         this.name = name;
         this.price = price;
+        this.markdown = 0.0f;
     }
 
     public static Item of(String name, float price) {
         return new Item(name, price);
+    }
+
+    public float calculatePrice() {
+        return price - markdown;
     }
 }
--- src/test/java/com/zimmeren/checkout/ItemTest.java
+++ src/test/java/com/zimmeren/checkout/ItemTest.java
@@ -15,4 +15,23 @@ public class ItemTest {
         assertEquals(price, cookies.price, 0);
     }
 
+    @Test
+    public void whenNoDiscountsAreAppliedCalculateItemPriceReturnsTheRegularPrice() {
+        String name = "Cookies";
+        float price = 3.50f;
+        Item cookies = Item.of(name, price);
+        assertEquals(price, cookies.calculatePrice(), 0);
+    }
+
+    @Test
+    public void whenSettingAMarkdownTheCalculatePriceItemReturnsThePriceMinusMarkdown() {
+        String name = "Cookies";
+        float price = 3.50f;
+        float markdown = 0.75f;
+        Item cookies = Item.of(name, price);
+        cookies.markdown = markdown;
+        assertEquals(price - markdown, cookies.calculatePrice(), 0);
+
+    }
+
 }
