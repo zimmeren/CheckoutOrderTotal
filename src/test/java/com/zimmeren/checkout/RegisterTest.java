e799fea4c6fce61f84ff8ab21d8b6bbb7740292a
unknown <zimmeren@umich.edu>
2019-01-28 22:49:31 -0500

    cleanup variable use and consistency in Register Test

--- src/test/java/com/zimmeren/checkout/RegisterTest.java
+++ src/test/java/com/zimmeren/checkout/RegisterTest.java
@@ -11,17 +11,16 @@ public class RegisterTest {
     @Test
     public void whenThereHaveBeenNoPurchasesThenTotalIsZero() {
         Register register = new Register();
-        assertEquals(0.0f, register.getTotal(), 0);
+        assertEquals(0.00f, register.getTotal(), 0);
     }
 
     @Test
     public void whenAddingPurchaseTotalIncreases() {
         Register register = new Register();
         String item = "cookies";
-        float price = 3.50f;
-        register.catalog.addItem(item, price);
+        register.catalog.addItem(item, 3.50f);
         register.purchaseEaches(item);
-        assertEquals(price, register.getTotal(), 0);
+        assertEquals(3.50f, register.getTotal(), 0);
     }
 
     @Test (expected = NoSuchElementException.class)
@@ -34,11 +33,10 @@ public class RegisterTest {
     public void whenRemovingPurchaseTotalDecreases() {
         Register register = new Register();
         String item = "cookies";
-        float price = 3.50f;
-        register.catalog.addItem(item, price);
+        register.catalog.addItem(item, 3.50f);
         register.purchaseEaches(item);
         register.removeEaches(item);
-        assertEquals(0.0f, register.getTotal(), 0);
+        assertEquals(0.00f, register.getTotal(), 0);
     }
 
     @Test (expected = NoSuchElementException.class)
@@ -51,34 +49,30 @@ public class RegisterTest {
     public void whenPurchasingMarkedDownItemTheTotalPriceUsesTheMarkdownPrice() {
         Register register = new Register();
         String item = "cookies";
-        float price = 3.50f;
-        float markdown = 0.75f;
-        register.catalog.addItem(item, price);
+        register.catalog.addItem(item, 3.50f);
         register.catalog.setItemMarkdown(item, 0.75f);
         register.purchaseEaches(item);
-        assertEquals(price - markdown, register.getTotal(), 0);
+        assertEquals(2.75f, register.getTotal(), 0);
     }
 
     @Test
     public void whenPurchasingMultipleOfAnItemTheTotalPriceIsForThatQuantity() {
         Register register = new Register();
         String item = "cookies";
-        float price = 3.50f;
-        register.catalog.addItem(item, price);
+        register.catalog.addItem(item, 3.50f);
         register.purchaseEaches(item);
         register.purchaseEaches(item);
         register.purchaseEaches(item);
         register.purchaseEaches(item);
         register.purchaseEaches(item);
-        assertEquals(price * 5, register.getTotal(), 0);
+        assertEquals(17.50f, register.getTotal(), 0);
     }
 
     @Test
     public void whenMultipleItemsHaveBeenPurchasedSomeCanBeRemovedAndTheTotalPriceIsForThatQuantity() {
         Register register = new Register();
         String item = "cookies";
-        float price = 3.50f;
-        register.catalog.addItem(item, price);
+        register.catalog.addItem(item, 3.50f);
         register.purchaseEaches(item);
         register.purchaseEaches(item);
         register.purchaseEaches(item);
@@ -86,42 +80,35 @@ public class RegisterTest {
         register.purchaseEaches(item);
         register.removeEaches(item);
         register.removeEaches(item);
-        assertEquals(price * 3, register.getTotal(), 0);
+        assertEquals(10.50f, register.getTotal(), 0);
     }
 
     @Test
     public void whenPurchasingAWeightedItemTheTotalPriceIsForThatWeight() {
         Register register = new Register();
         String item = "chicken fingers";
-        float price = 2.98f;
-        register.catalog.addItem(item, price);
-        float weight = 0.47f;
-        register.purchaseWeighted(item, weight);
-        assertEquals(1.4f, register.getTotal(), 0);
+        register.catalog.addItem(item, 2.98f);
+        register.purchaseWeighted(item, 0.47f);
+        assertEquals(1.40f, register.getTotal(), 0);
     }
 
     @Test
     public void whenRemovingWeightedItemTheTotalPriceReturnsToZero() {
         Register register = new Register();
         String item = "chicken fingers";
-        float price = 2.98f;
-        register.catalog.addItem(item, price);
-        float weight = 0.47f;
-        register.purchaseWeighted(item, weight);
-        register.removeWeighted(item, weight);
-        assertEquals(0f, register.getTotal(), 0);
+        register.catalog.addItem(item, 2.98f);
+        register.purchaseWeighted(item, 0.47f);
+        register.removeWeighted(item, 0.47f);
+        assertEquals(0.00f, register.getTotal(), 0);
     }
 
     @Test
     public void whenRemovingPartialWeightedItemTheTotalPriceReturnsThePartialAmountLeft() {
         Register register = new Register();
         String item = "chicken fingers";
-        float price = 2.98f;
-        register.catalog.addItem(item, price);
-        float weight = 0.47f;
-        register.purchaseWeighted(item, weight);
-        float removeWeight = 0.12f;
-        register.removeWeighted(item, removeWeight);
+        register.catalog.addItem(item, 2.98f);
+        register.purchaseWeighted(item, 0.47f);
+        register.removeWeighted(item, 0.12f);
         assertEquals(1.05f, register.getTotal(), 0);
     }
 
@@ -129,36 +116,31 @@ public class RegisterTest {
     public void whenRemovingMoreWeightedItemThanPurchasedItWillProtectItselfAndReturnZero() {
         Register register = new Register();
         String item = "chicken fingers";
-        float price = 2.98f;
-        register.catalog.addItem(item, price);
-        float weight = 0.47f;
-        register.purchaseWeighted(item, weight);
-        float removeWeight = 0.72f;
-        register.removeWeighted(item, removeWeight);
-        assertEquals(0f, register.getTotal(), 0);
+        register.catalog.addItem(item,  2.98f);
+        register.purchaseWeighted(item, 0.47f);
+        register.removeWeighted(item, 0.72f);
+        assertEquals(0.00f, register.getTotal(), 0);
     }
 
     @Test
     public void whenAddingBuyGetToEachesItemTheSpecialPriceWillBeInTotalPriceOnceQualified() {
         Register register = new Register();
         String item = "doritos";
-        float price = 1.67f;
-        register.catalog.addItem(item, price);
+        register.catalog.addItem(item, 1.67f);
         register.catalog.setItemSpecial(item, 1f, 1f, 1f);
         register.purchaseEaches(item);
-        assertEquals(price, register.getTotal(), 0);
+        assertEquals(1.67f, register.getTotal(), 0);
         register.purchaseEaches(item);
-        assertEquals(price, register.getTotal(), 0);
+        assertEquals(1.67f, register.getTotal(), 0);
         register.purchaseEaches(item);
-        assertEquals(2*price, register.getTotal(), 0);
+        assertEquals(3.34f, register.getTotal(), 0);
     }
 
     @Test
     public void whenAddingBuyGetWithLimitToEachesItemTheSpecialPriceWillBeInTotalPriceOnceQualified() {
         Register register = new Register();
         String item = "doritos";
-        float price = 1.67f;
-        register.catalog.addItem(item, price);
+        register.catalog.addItem(item, 1.67f);
         register.catalog.setItemSpecialWithLimit(item, 1f, 1f, 1f, 1);
         register.purchaseEaches(item);
         register.purchaseEaches(item);
@@ -171,11 +153,10 @@ public class RegisterTest {
     public void whenAddingBuyGetToWeightedItemTheSpecialPriceWillBeInTotalPriceOnceQualified() {
         Register register = new Register();
         String item = "ground beef";
-        float price = 1.49f;
-        register.catalog.addItem(item, price);
+        register.catalog.addItem(item, 1.49f);
         register.catalog.setItemSpecial(item, 0.5f, 0.25f, 0.25f);
         register.purchaseWeighted(item, 0.49f);
-        assertEquals(.73f, register.getTotal(), 0);
+        assertEquals(0.73f, register.getTotal(), 0);
         register.purchaseWeighted(item, 0.52f);
         assertEquals(1.42f, register.getTotal(), 0);
     }
@@ -184,8 +165,7 @@ public class RegisterTest {
     public void whenAddingBuyGetWithLimitToWeightedItemTheSpecialPriceWillBeInTotalPriceOnceQualified() {
         Register register = new Register();
         String item = "ground beef";
-        float price = 1.49f;
-        register.catalog.addItem(item, price);
+        register.catalog.addItem(item, 1.49f);
         register.catalog.setItemSpecialWithLimit(item, 0.5f, 0.25f, 0.25f, 1);
         register.purchaseWeighted(item, 0.67f);
         register.purchaseWeighted(item, 0.52f);
@@ -196,15 +176,14 @@ public class RegisterTest {
     public void whenAddingBuyForToEachesItemTheSpecialPriceWillBeInTotalPriceOnceQualified() {
         Register register = new Register();
         String item = "doritos";
-        float price = 1.67f;
-        register.catalog.addItem(item, price);
+        register.catalog.addItem(item, 1.67f);
         register.catalog.setItemSpecial(item, 3f, 3f);
         register.purchaseEaches(item);
-        assertEquals(price, register.getTotal(), 0);
+        assertEquals(1.67f, register.getTotal(), 0);
         register.purchaseEaches(item);
-        assertEquals(2*price, register.getTotal(), 0);
+        assertEquals(3.34f, register.getTotal(), 0);
         register.purchaseEaches(item);
-        assertEquals(3f, register.getTotal(), 0);
+        assertEquals(3.00f, register.getTotal(), 0);
         register.purchaseEaches(item);
         assertEquals(4.67f, register.getTotal(), 0);
     }
@@ -213,8 +192,7 @@ public class RegisterTest {
     public void whenAddingBuyForWithLimitToEachesItemTheSpecialPriceWillBeInTotalPriceOnceQualified() {
         Register register = new Register();
         String item = "doritos";
-        float price = 1.67f;
-        register.catalog.addItem(item, price);
+        register.catalog.addItem(item, 1.67f);
         register.catalog.setItemSpecialWithLimit(item, 2f, 3f, 1);
         register.purchaseEaches(item);
         register.purchaseEaches(item);
@@ -227,8 +205,7 @@ public class RegisterTest {
     public void whenAddingBuyForToWeightedItemTheSpecialPriceWillBeInTotalPriceOnceQualified() {
         Register register = new Register();
         String item = "ground beef";
-        float price = 1.49f;
-        register.catalog.addItem(item, price);
+        register.catalog.addItem(item, 1.49f);
         register.catalog.setItemSpecial(item, 2.5f, 3.02f);
         register.purchaseWeighted(item, 2.02f);
         assertEquals(3.01f, register.getTotal(), 0);
@@ -240,8 +217,7 @@ public class RegisterTest {
     public void whenAddingBuyForWithLimitToWeightedItemTheSpecialPriceWillBeInTotalPriceOnceQualified() {
         Register register = new Register();
         String item = "ground beef";
-        float price = 1.49f;
-        register.catalog.addItem(item, price);
+        register.catalog.addItem(item, 1.49f);
         register.catalog.setItemSpecialWithLimit(item, 2.5f, 3.02f, 1);
         register.purchaseWeighted(item, 3f);
         register.purchaseWeighted(item, 3f);
@@ -252,8 +228,7 @@ public class RegisterTest {
     public void whenRemovingAPurchasedEachesItemTheBuyForSpecialIsInvalidated() {
         Register register = new Register();
         String item = "chaco taco";
-        float price = .99f;
-        register.catalog.addItem(item, price);
+        register.catalog.addItem(item, 0.99f);
         register.catalog.setItemSpecial(item, 3f, 2.49f);
         register.purchaseEaches(item);
         register.purchaseEaches(item);
@@ -267,8 +242,7 @@ public class RegisterTest {
     public void whenRemovingAPurchasedWeightedItemTheBuyGetSpecialIsInvalidated() {
         Register register = new Register();
         String item = "roasted chicken";
-        float price = 3.99f;
-        register.catalog.addItem(item, price);
+        register.catalog.addItem(item, 3.99f);
         register.catalog.setItemSpecial(item, 1.5f, 0.5f, 1f);
         register.purchaseWeighted(item, 2f);
         assertEquals(5.99f, register.getTotal(), 0);
