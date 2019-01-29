742977406c81fa0fb34e6e2338843040fbc9e58e
unknown <zimmeren@umich.edu>
2019-01-28 22:57:36 -0500

    cleanup variable use, consistency, and redundent asserts in Catalog Test

--- src/test/java/com/zimmeren/checkout/CatalogTest.java
+++ src/test/java/com/zimmeren/checkout/CatalogTest.java
@@ -12,18 +12,15 @@ public class CatalogTest {
     public void whenAddingItemItsPriceCanBeFound() {
         Catalog catalog = new Catalog();
         String item = "cookies";
-        float price = 3.50f;
-        catalog.addItem(item, price);
-        assertEquals(price, catalog.getItemPrice(item), 0);
+        catalog.addItem(item, 3.50f);
+        assertEquals(3.50f, catalog.getItemPrice(item), 0);
     }
 
     @Test (expected = NoSuchElementException.class)
     public void whenRemovingItemItsPriceCannotBeFound() {
         Catalog catalog = new Catalog();
         String item = "cookies";
-        float price = 3.50f;
-        catalog.addItem(item, price);
-        assertEquals(price, catalog.getItemPrice(item), 0);
+        catalog.addItem(item, 3.50f);
         catalog.removeItem(item);
         catalog.getItemPrice(item);
     }
@@ -38,79 +35,70 @@ public class CatalogTest {
     public void whenUpdatingItemItsPriceIsUpdated() {
         Catalog catalog = new Catalog();
         String item = "cookies";
-        float price = 3.50f;
-        catalog.addItem(item, price);
-        assertEquals(price, catalog.getItemPrice(item), 0);
-        float updatedPrice = 3.00f;
-        catalog.setItemPrice(item, updatedPrice);
-        assertEquals(updatedPrice, catalog.getItemPrice(item), 0);
+        catalog.addItem(item, 3.50f);
+        catalog.setItemPrice(item, 3.00f);
+        assertEquals(3.00f, catalog.getItemPrice(item), 0);
     }
 
     @Test (expected = NoSuchElementException.class)
     public void whenUpdatingNonExistentItemExceptionIsThrown() {
         Catalog catalog = new Catalog();
-        catalog.setItemPrice("fake item", 0.0f);
+        catalog.setItemPrice("fake item", 0.00f);
     }
 
     @Test
     public void whenSettingItemMarkdownItsPriceIsUpdated() {
         Catalog catalog = new Catalog();
         String item = "cookies";
-        float price = 3.50f;
-        catalog.addItem(item, price);
+        catalog.addItem(item, 3.50f);
         float markdown = 0.75f;
         catalog.setItemMarkdown(item, markdown);
-        assertEquals(price - markdown, catalog.getItemPrice(item), 0);
+        assertEquals(2.75f, catalog.getItemPrice(item), 0);
     }
 
     @Test (expected = NoSuchElementException.class)
     public void whenSettingMarkdownOnNonExistentItemExceptionIsThrown() {
         Catalog catalog = new Catalog();
-        catalog.setItemMarkdown("fake item", 0.5f);
+        catalog.setItemMarkdown("fake item", 0.50f);
     }
 
     @Test
     public void whenGettingPriceOfMultipleOfAnItemReturnsTheMultipliedCost() {
         Catalog catalog = new Catalog();
         String item = "cookies";
-        float price = 3.50f;
-        catalog.addItem(item, price);
-        float quantity = 5;
-        assertEquals(price * quantity, catalog.getItemsPrice(item, quantity), 0);
+        catalog.addItem(item, 3.50f);
+        assertEquals(17.50f, catalog.getItemsPrice(item, 5f), 0);
     }
 
     @Test (expected = NoSuchElementException.class)
     public void whenGettingPriceOfMultipleOfAnItemOnNonExistentItemExceptionIsThrown() {
         Catalog catalog = new Catalog();
-        catalog.getItemsPrice("fake item", 5);
+        catalog.getItemsPrice("fake item", 5f);
     }
 
     @Test
     public void whenSettingNewBuyGetSpecialForItemThePriceReflectsSpecial() {
         Catalog catalog = new Catalog();
         String item = "fish sticks";
-        float price = 2.49f;
-        catalog.addItem(item, price);
+        catalog.addItem(item, 2.49f);
         catalog.setItemSpecial(item, 1f, 1f, 1f);
-        assertEquals(price, catalog.getItemsPrice(item, 2), 0);
+        assertEquals(2.49f, catalog.getItemsPrice(item, 2f), 0);
     }
 
     @Test
     public void whenSettingNewBuyGetWithLimitSpecialForItemThePriceReflectsSpecial() {
         Catalog catalog = new Catalog();
         String item = "fish sticks";
-        float price = 2.49f;
-        catalog.addItem(item, price);
+        catalog.addItem(item, 2.49f);
         catalog.setItemSpecialWithLimit(item, 1f, 1f, 1f, 1);
-        assertEquals(7.47f, catalog.getItemsPrice(item, 4), 0);
+        assertEquals(7.47f, catalog.getItemsPrice(item, 4f), 0);
     }
 
     @Test
     public void whenSettingNewBuyForSpecialForItemThePriceReflectsSpecial() {
         Catalog catalog = new Catalog();
         String item = "mozzi sticks";
-        float price = 5.99f;
-        catalog.addItem(item, price);
+        catalog.addItem(item, 5.99f);
         catalog.setItemSpecial(item, 3f, 10.99f);
         assertEquals(21.98f, catalog.getItemsPrice(item, 6f), 0);
     }
@@ -119,8 +107,7 @@ public class CatalogTest {
     public void whenSettingNewBuyForWithLimitSpecialForItemThePriceReflectsSpecial() {
         Catalog catalog = new Catalog();
         String item = "mozzi sticks";
-        float price = 5.99f;
-        catalog.addItem(item, price);
+        catalog.addItem(item, 5.99f);
         catalog.setItemSpecialWithLimit(item, 3f, 10.99f, 1);
         assertEquals(28.96f, catalog.getItemsPrice(item, 6f), 0);
     }
@@ -129,11 +116,10 @@ public class CatalogTest {
     public void whenUpdatingASpecialOnAnItemThePriceReflectsNewSpecial() {
         Catalog catalog = new Catalog();
         String item = "fish sticks";
-        float price = 2.49f;
-        catalog.addItem(item, price);
+        catalog.addItem(item, 2.49f);
         catalog.setItemSpecial(item, 1f, 1f, 1f);
         catalog.setItemSpecial(item, 1f, 1f, 0.5f);
-        assertEquals(3.74f, catalog.getItemsPrice(item, 2), 0);
+        assertEquals(3.74f, catalog.getItemsPrice(item, 2f), 0);
     }
 
     @Test (expected = NoSuchElementException.class)
@@ -164,11 +150,10 @@ public class CatalogTest {
     public void whenRemovingASpecialFromItemThePriceReflectsRegularPrice() {
         Catalog catalog = new Catalog();
         String item = "fish sticks";
-        float price = 2.49f;
-        catalog.addItem(item, price);
+        catalog.addItem(item, 2.49f);
         catalog.setItemSpecial(item, 1f, 1f, 1f);
         catalog.removeItemSpecial(item);
-        assertEquals(4.98f, catalog.getItemsPrice(item, 2), 0);
+        assertEquals(4.98f, catalog.getItemsPrice(item, 2f), 0);
     }
 
     @Test (expected = NoSuchElementException.class)
